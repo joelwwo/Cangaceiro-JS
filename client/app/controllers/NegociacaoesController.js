7e3c4400844691455ad071b998294753534e3afa
joelwwo <joeldossantossilva21@gmail.com>
2020-01-10 14:38:25 -0300

    Até o início do capítulo 4.

--- client/app/controllers/NegociacaoesController.js
+++ client/app/controllers/NegociacaoesController.js
@@ -1,17 +1,68 @@
 class NegociacaoController {
 
+    constructor() {
+
+        let $ = document.querySelector.bind(document)
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+    }
+
     adiciona(event) {
 
         event.preventDefault();
 
-        let $ = sel => document.querySelector(sel)
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+        //let data = new Date(this._inputData.value.split('-'));
+
+        //let data = new Date(this._inputData.value.replace(/-/g, ','));
+
+        // dessa forma o mês fica sempre um a mais, 0 (janeiro) ao 11 (dezembro)
+        //let data = new Date(...this._inputData.value.split('-'));
+
+        /* let data = new Date(
+            ...this._inputData.value
+                .split('-')
+                .map((item, indice) => {
+                    if (indice == 1) {
+                        return item - 1
+                    }
+                    return item;
+                })
+        ); */
+
+        /* let data = new Date(
+            ...this._inputData.value
+                .split('-')
+                .map((item, indice) => {
+                    return item - indice % 2;
+                })
+        ); */
+
+        /* let data = new Date(
+            ...this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2)
+        );
+
+        console.log(data.toDateString()); */
+
+        let converter = new DataConverter();
+
+        let data = converter.paraData(this._inputData.value);
+
+        let negociacao = new Negociacao(
+            data,
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        )
+
+        let diaMesAno = converter.paraTexto(data)
+
+        console.log(negociacao)
+
+        console.log(diaMesAno);
         
-        console.log(inputData.value);
-        console.log(parseInt(inputQuantidade.value));
-        console.log(parseFloat(inputValor.value));
 
 
 
