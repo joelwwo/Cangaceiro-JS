20a6157159ef4ccf070e7334e5ff20ea7b5e3dd9
joelwwo <joeldossantossilva21@gmail.com>
2020-01-10 16:58:45 -0300

    Finalização do capitulo 4, manipulação de datas.

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -1,14 +1,19 @@
 class DataConverter {
 
+    constructor() {
+        throw new Error(`A classe DateConverter não pode se instanciada.`)
+    }
+
     static paraTexto(data) {
 
-        return data.getDate()
-            + '/' + (data.getMonth() + 1)
-            + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`
 
     }
 
     static paraData(texto) {
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto))
+            throw new Error('A data deve estar no formato aaaa-mm-dd');
+
         return new Date(...texto.split('-').map((item, indice) =>
             item - indice % 2));
     }
