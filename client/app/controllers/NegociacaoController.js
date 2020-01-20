727350a43c2da6d30f1faea3b2dbcdacc0b165b5
joelwwo <joeldossantossilva21@gmail.com>
2020-01-20 14:58:01 -0300

    pag 246 - Trabalhando com Proxy

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -2,40 +2,25 @@ class NegociacaoController {
 
     constructor() {
 
-        const $ = document.querySelector.bind(document)
+        const $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        const self = this;
-
-        this._negociacoes = new Proxy(new Negociacoes(), {
-
-            get(target, prop, receiver) {
-
-                if (typeof target[prop] == typeof Function && ['adiciona', 'esvazia'].includes(prop)) {
-
-                    return function () {
-                        console.log(`"${prop}" disparou a armadilha`);
-                        target[prop].apply(target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-
-                }
-                else {
-
-                    return target[prop]
-
-                }
-
-
-            }
-
-        });
+        this._negociacoes = ProxyFactory.create(
+            new Negociacoes(),
+            ['adiciona', 'esvazia'],
+            model => this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView('#negociacoes');
         this._negociacoesView.update(this._negociacoes);
-        this._mensagem = new Mensagem();
+
+        this._mensagem = ProxyFactory.create(
+            new Mensagem(),
+            ['texto'],
+            model => this._mensagemView.update(model)
+        );
+
         this._mensagemView = new MensagemView('#mensagemView');
         this._mensagemView.update(this._mensagem);
 
@@ -68,7 +53,6 @@ class NegociacaoController {
 
         this._negociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._negociacoesView.update(this._negociacoes);
         this._limpaFormulario();
 
     }
@@ -77,7 +61,6 @@ class NegociacaoController {
 
         this._negociacoes.esvazia();
         this._mensagem.texto = 'Negociações excluídas com sucesso!';
-        this._mensagemView.update(this._mensagem);
 
     }
 
